3c10e4d3adcdd9f0f7b1d07709365c24b181c86e
Tejas <tmehta@redhat.com>
2012-07-24 16:19:26 -0400

    Added Updated kitchensink-spring

---
+++ kitchensink-spring/src/main/java/org/jboss/spring/quickstarts/kitchensink/kitchensink_spring/repo/MemberDao.java
@@ -0,0 +1,16 @@
+package org.jboss.spring.quickstarts.kitchensink.kitchensink_spring.repo;
+
+import java.util.List;
+
+import org.jboss.spring.quickstarts.kitchensink.kitchensink_spring.domain.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
